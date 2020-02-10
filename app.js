f094e72682dc39003012796d7183984049af5c84
simarsidhu03 <59624251+simarsidhu03@users.noreply.github.com>
2020-02-10 14:13:38 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const green = 'rgba(118,174, 170, 1)'
+const black = 'rgba(23,35, 50, 1)'
+const brown = 'rgba(205,164, 133, 1)'
+const greenblue = 'rgba(62,171, 201, 1)'
+const pink = 'rgba(254,156, 161, 1)'
+const blue = 'rgba(98,189, 254, 1)'
+
+const data = [
+    {
+        topic: 'Food',
+        title: 'Wake Up and Smell the Coffee',
+        price: '$0.90',
+        color: green
+    },
+    {
+        topic: 'Architecture',
+        title: 'The Brand New NASA Office',
+        price: '$0.19',
+        color: black
+    },
+    {
+        topic: 'Travel',
+        title: 'Experience the Saharan Sands',
+        price: '$2.29',
+        color: brown
+    },
+    {
+        topic: 'Interior',
+        title: '9 Air-Cleaning Plants Your Home Needs',
+        price: '$0.09',
+        color: greenblue
+    },
+    {
+        topic: 'Food',
+        title: 'One Month Sugar Detox',
+        price: '$0.99',
+        color: pink
+    },
+    {
+        topic: 'Photography',
+        title: 'Shooting Minimal Instagram Photos',
+        price: '$0.29',
+        color: blue
+    }
+]
+
+// Instructions
+// Take the data above and display it as tiles on the page
+
+let ndata = document.querySelector('main')
+let nhtml=``
+for(elements of data){
+    nhtml = nhtml+`<section class='card' style="background-color: ${elements.color}">
+    <h3> ${elements.topic}</h3>
+    <h2> ${elements.title}</h2>
+    <button> Read for ${elements.price}</button>
+    </section>`
+}
+ndata.innerHTML=nhtml
+
+
